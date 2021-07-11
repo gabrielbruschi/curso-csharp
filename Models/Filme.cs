651997e089c5ead5f0101f4081e30db59ad2b83c
Gabriel Bruschi <gabrielbruschi1@gmail.com>
2021-07-11 19:56:21 -0300

    Update Filme.cs

--- Models/Filme.cs
+++ Models/Filme.cs
@@ -1,13 +1,13 @@
 public class Filme
 {
-    public Filme(string titulo)
+    public long Id { get; set; }
+    public string Titulo { get; set; }
+    public long DiretorId { get; set; }
+    public Diretor Diretor { get; set; }
+
+    public Filme(string titulo, long diretorId)
     {
         Titulo = titulo;
+        DiretorId = diretorId;
     }
-    public int Id { get; set; }
-    public string Titulo { get; set; }
-    public string Ano { get; set; }
-    public string Genero { get; set; }
-    public long DiretorId { get; set; } //PK do diretor
-    public Diretor Diretor { get; set; } //obj Diretor; dentro do bd não tem esse diretor;
 }
