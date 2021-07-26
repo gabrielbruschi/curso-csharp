ddbc2e45f0eff7ab6c01b3c68c8e7e90cfc8407b
Gabriel Bruschi <gabrielbruschi1@gmail.com>
2021-07-25 22:31:05 -0300

    Add Paginacao ao Projeto

---
+++ Extensions/DataPagerExtension.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+public static class DataPagerExtension
+{
+    public static async Task<PagedModel<TModel>> PaginateAsync<TModel>(
+    this IQueryable<TModel> query,
+    int page,
+    int limit,
+    CancellationToken cancellationToken)
+    where TModel : class
+    {
+
+        var paged = new PagedModel<TModel>();
+
+        page = (page < 0) ? 1 : page;
+
+        paged.CurrentPage = page;
+        paged.PageSize = limit;
+
+        var totalItemsCountTask = query.CountAsync(cancellationToken);
+
+        var startRow = (page - 1) * limit;
+        paged.Items = await query
+                    .Skip(startRow)
+                    .Take(limit)
+                    .ToListAsync(cancellationToken);
+
+        paged.TotalItems = await totalItemsCountTask;
+        paged.TotalPages = (int)Math.Ceiling(paged.TotalItems / (double)limit);
+
+        return paged;
+    }
+}
---
+++ Models/PageModel.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+public class PagedModel<TModel>
+{
+    const int MaxPageSize = 5;
+    private int _pageSize;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
+    }
+
+    public int CurrentPage { get; set; }
+    public int TotalItems { get; set; }
+    public int TotalPages { get; set; }
+    public IList<TModel> Items { get; set; }
+
+    public PagedModel()
+    {
+        Items = new List<TModel>();
+    }
+}
