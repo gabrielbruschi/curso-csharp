e011c3cd947e93c0e6342da6608d86d37c1afb3e
Gabriel Bruschi <gabrielbruschi1@gmail.com>
2021-08-02 21:21:16 -0300

    Update FilmeController.cs

--- Controllers/FilmeController.cs
+++ Controllers/FilmeController.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+[Authorize]
 [ApiController]
 [Route("[controller]")]
 public class FilmeController : ControllerBase
