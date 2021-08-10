f53a1b69b99d8e3f27fea478de1987e45e8665c5
Gabriel Bruschi <gabrielbruschi1@gmail.com>
2021-08-09 21:13:25 -0300

    Adiciona projeto de testes unitarios

---
+++ MeuAppTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+
+namespace MeuAppTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, Add(2, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(2)]
+        [InlineData(1)]       
+        [InlineData(0)]
+        public void MyFirstTheory(int value)
+        {
+            Assert.True(MenorQueQuatro(value));
+        }
+
+        bool MenorQueQuatro(int value)
+        {
+            return value < 4;
+        }
+    }
+}
